0719bb9c21c3a6c33abcbd2dadf433cf9095d3b0
Dr. ERDI Gergo <gergo@erdi.hu>
2017-05-01 14:42:35 +0800

    Add port C, and all the PIN registers

--- src/avr.rs
+++ src/avr.rs
@@ -1,6 +1,12 @@
+pub const PINB:   *mut u8  = 0x23 as *mut u8;
 pub const DDRB:   *mut u8  = 0x24 as *mut u8;
 pub const PORTB:  *mut u8  = 0x25 as *mut u8;
 
+pub const PINC:   *mut u8  = 0x26 as *mut u8;
+pub const DDRC:   *mut u8  = 0x27 as *mut u8;
+pub const PORTC:  *mut u8  = 0x28 as *mut u8;
+
+pub const PIND:   *mut u8  = 0x29 as *mut u8;
 pub const DDRD:   *mut u8  = 0x2a as *mut u8;
 pub const PORTD:  *mut u8  = 0x2b as *mut u8;
 
