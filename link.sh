d479c99dacc8df915fa48fdb9c156987ad5903f7
Dr. ERDI Gergo <gergo@erdi.hu>
2017-06-17 11:28:46 +0800

    Fix linking configuration in target .JSON file

--- link.sh
+++ link.sh
@@ -4,5 +4,5 @@ OBJS="$BASE/deps/*.o $BASE/deps/*.rlib"
 LDSCRIPT=lookup-text.ld
 
 avr-gcc -Os -Wl,--gc-sections -mmcu=atmega328p -T $LDSCRIPT -o $BASE/image.elf $OBJS
-avr-strip $BASE/image.elf
+# avr-strip $BASE/image.elf
 avr-objcopy -Oihex -R.eeprom $BASE/image.elf $BASE/image.hex
--- roms.sh
+++ roms.sh
@@ -1,1 +1,4 @@
-avr-gcc -O3 -mmcu=atmega328p -fno-exceptions -ffunction-sections -fdata-sections -c src/rom.c -o target/avr-atmega328p/release/deps/rom.o
+DEST="target/avr-atmega328p/release/deps"
+mkdir -p "$DEST"
+
+avr-gcc -O3 -mmcu=atmega328p -fno-exceptions -ffunction-sections -fdata-sections -c src/rom.c -o "$DEST/rom.o"
