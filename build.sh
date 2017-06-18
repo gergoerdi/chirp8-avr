ecd151c84a9e32ddaec71007610dce104d2b6d07
Dr. ERDI Gergo <gergo@erdi.hu>
2017-06-18 20:04:25 +0800

    Use Xargo to link to libcore-mini

---
+++ build.sh
@@ -0,0 +1,3 @@
+sh roms.sh
+XARGO_RUST_SRC=/ xargo build --target=avr-atmega328p --release $@
+
--- link.sh
+++
@@ -1,8 +0,0 @@
-BASE=target/avr-atmega328p/release
-CRATE=hello-avr
-OBJS="$BASE/deps/*.o $BASE/deps/*.rlib"
-LDSCRIPT=lookup-text.ld
-
-avr-gcc -Os -Wl,--gc-sections -mmcu=atmega328p -T $LDSCRIPT -o $BASE/image.elf $OBJS
-# avr-strip $BASE/image.elf
-avr-objcopy -Oihex -R.eeprom $BASE/image.elf $BASE/image.hex
