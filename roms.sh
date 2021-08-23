ceab82c9e33911eea8dac3a77f17f66ba827bb70
Dr. ERDI Gergo <gergo@erdi.hu>
2021-08-23 21:02:20 +0800

    Update to Rust 1.47.0-nightly (2d8a3b918 2020-08-26)

--- roms.sh
+++ roms.sh
@@ -1,4 +1,4 @@
-DEST="target/avr-atmega328p/release/deps"
+DEST="target/avr-unknown-gnu-atmega328/release/deps"
 mkdir -p "$DEST"
 
 avr-gcc -O3 -mmcu=atmega328p -fno-exceptions -ffunction-sections -fdata-sections -c src/rom.c -o "$DEST/rom.o"
