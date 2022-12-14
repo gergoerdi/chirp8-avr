11702c481fa7647545676d55f324f28cd7064876
Gergo ERDI <gergo@erdi.hu>
2022-12-14 20:23:33 +0800

    Use Ruduino for SPI

--- src/spi.rs
+++ src/spi.rs
@@ -1,23 +1,19 @@
 use ruduino::Pin;
-use ruduino::Register;
-use ruduino::cores::current::{port,SPCR,SPDR,SPSR};
+use ruduino::cores::current::*;
+use ruduino::modules::HardwareSpi;
 
 pub fn setup() {
     // Set up SPI pin directions
-    port::B3::set_output();
-    port::B5::set_output();
-    port::B4::set_input();
-
-    // SS is used for unrelated output
-    port::B2::set_output();
-    port::B2::set_high();
+    <Spi as HardwareSpi>::MasterInSlaveOut::set_input();
+    <Spi as HardwareSpi>::MasterOutSlaveIn::set_output();
+    <Spi as HardwareSpi>::Clock::set_output();
 
     // Turn on SPI
-    SPCR::set(SPCR::SPE | SPCR::MSTR);
+    <Spi as HardwareSpi>::set_master();
+    <Spi as HardwareSpi>::enable();
 }
 
 pub fn sync(out: u8) -> u8 {
-    SPDR::write(out);
-    SPSR::wait_until_set(SPSR::SPIF);
-    SPDR::read()
+    <Spi as HardwareSpi>::send_byte(out);
+    <Spi as HardwareSpi>::receive_byte()
 }
